2580333e817cc8edd0386f5c78e9fa366144265f
Mattia Rigotti <matrig@users.noreply.github.com>
2026-03-25 10:42:04 +0100

    chore: select wind indices for next compute run

--- scripts/compute_indices.py
+++ scripts/compute_indices.py
@@ -29,7 +29,7 @@ FREQ = "day"
 #  SELECTED_INDICES = ["solidprcptot_winter", "solidprcptot_year"]  # Snow indices only
 #  SELECTED_INDICES = ["rx1day", "cdd", "cwd"]  # Precipitation extremes only
 #  SELECTED_INDICES = ["tg_mean_year", "tx_mean_year", "tn_mean_year"]  # Temperature means only
-SELECTED_INDICES = ["par_plant_level", "spei3_severe_prob"]
+SELECTED_INDICES = ["sfcwind_year", "sfcwind10", "sfcwind20"]
 # SELECTED_INDICES = None
 
 # Dask configuration
